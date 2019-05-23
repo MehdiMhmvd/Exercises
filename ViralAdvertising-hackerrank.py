b63abfc54437ba10dcd23eddaabe6da08730b66a
mehdimhmvd <Mehdi#95251>
2019-05-23 08:09:36 -0700

    floor was changed with round func

--- ViralAdvertising-hackerrank.py
+++ ViralAdvertising-hackerrank.py
@@ -1,8 +1,9 @@
-n = 50
+import math
+n = 50  #1379192761
 shared = 5
 comulative = 0
 for i in range (1 ,n+1):
-	liked = round (shared / 2)
+	liked = math.floor(shared / 2)
 	comulative += liked
 	shared = liked * 3
 	print("day %d = %d"%(i, comulative) )
