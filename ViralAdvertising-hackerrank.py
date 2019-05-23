714730d682927578a2b7e0c02f522b8e918a6e6e
mehdimhmvd <Mehdi#95251>
2019-05-23 07:43:26 -0700

    edited

--- ViralAdvertising-hackerrank.py
+++ ViralAdvertising-hackerrank.py
@@ -1,6 +1,8 @@
-n = 2
-liked = round(5/2)
-for i in range (1 ,n+1 ):
-	liked = 
-
-print(liked)
+n = 50
+shared = 5
+comulative = 0
+for i in range (1 ,n+1):
+	liked = round (shared / 2)
+	comulative += liked
+	shared = liked * 3
+	print("day %d = %d"%(i, comulative) )
