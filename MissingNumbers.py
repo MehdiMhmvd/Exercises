90b5b7c2a1560f17e512048078fe0428dccda7df
mehdimhmvd <Mehdi#95251>
2019-05-23 01:28:50 -0700

    imprivededited

--- MissingNumbers.py
+++ MissingNumbers.py
@@ -1,10 +1,14 @@
 '''
 this program get the list of numbers and compair it with the missing list and 
-return the missing items s
+return  sorted the missing items 
+
 '''
 
-arr = [203, 205, 206, 207, 208, 203, 204, 205, 206]
-brr = [203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204]
+#arr = [203, 205, 206, 207, 208, 203, 204, 205, 206]
+#brr = [203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204]
+arr = [11, 4, 11, 7, 13, 4 ,12 ,11 ,10 ,14]
+brr = [11, 4 ,11,8, 7 ,3,3, 7, 10, 13,13, 4, 8, 12, 11, 10,8 ,14, 12]
+
 
 OValues = {}
 NewValues = {}
@@ -15,8 +19,11 @@ for i in range(len(brr)):
 for n in range(len(arr)):
 	NewValues[arr[n]] = arr.count(arr[n])
 	
-for item in NewValues:
-	if NewValues[item] != OValues[item]:
+for item in OValues:
+	if item  not in NewValues.keys():
+		for m in range(OValues[item]):
+			diff.append(item)
+	elif NewValues[item] != OValues[item]:
 		d = OValues[item] - NewValues[item]
 		for i in range(d):
 			diff.append(item)
@@ -24,6 +31,6 @@ for item in NewValues:
 
 
  
-print(diff)
+print(sorted(diff))
 print(NewValues)	
 print(OValues)
