aab3ef70365ff48d26d3a4ce6e8f232510752833
mehdimhmvd <Mehdi#95251>
2019-05-24 04:21:54 -0700

    final log-test done

--- factorial-log.py
+++ factorial-log.py
@@ -1,6 +1,14 @@
 #1 usr/bin/python3
 import logging
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename = "myProgramLog.txt", level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+#1-Debug
+#2-INFO
+#3-WARNING
+#4- ERROR
+#5-CRITICAL
+#logging.disable(logging.DEBUG)     ##it will disable depends on level that we are passing to it 
+#logging.disable(logging.INFO)
+#logging.disable(logging.CRITICAL)  ## it will disbal eall the dubuges 
 
 logging.debug('Start of program')
 
@@ -10,6 +18,7 @@ def factorial(n):
 	for i in range(1 , n + 1):
 		total *= i
 		logging.debug('i is ' + str(i) + ', total is ' + str(total))
+		logging.info("this is logging for info")
 	logging.debug('End of factorial(%s)' %(n))
 	return total
 print(factorial(5))
