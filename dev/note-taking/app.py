d38cff46f1f6ba87d3212e4a8bf251586bf7c26a
mehdimhmvd <Mehdi#95251>
2019-06-02 00:41:58 -0700

    modified

--- dev/note-taking/app.py
+++ dev/note-taking/app.py
@@ -1,6 +1,16 @@
+import os 
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,"noter.db")
+db = SQLAlchemy(app)
+
+
+
 
 @app.route("/")
 @app.route("/index")
